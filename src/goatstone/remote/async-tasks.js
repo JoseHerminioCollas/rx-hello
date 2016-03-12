daa682cffeb88639eed728b9e8b465be0c619b21
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-03-12 15:37:11 -0800

    Make call to OpenWeatherUnderground.

--- src/goatstone/remote/async-tasks.js
+++ src/goatstone/remote/async-tasks.js
@@ -1,10 +1,12 @@
 /* goatstone.remote.async-tasks */
 'use strict'
 const async = require( "async" )
-//       taskFactory.get     getTask( 'twitter' )
+
 const twitterRemote = require( 'goatstone/remote/task/twitter-remote' )
 const countryRemote = require( 'goatstone/remote/task/country-remote' )
 const weatherRemote = require( 'goatstone/remote/task/weather-remote' )
+
+weatherRemote.q = 'Cleveland'
 
 const tasks = [ 
 	weatherRemote, 
@@ -19,3 +21,12 @@ async.parallel( tasks.map( e => {
 function onComplete(){
 	console.log("complete. . .")
 }
+
+/*   
+get the weather
+get twitter data about the condition
+get some country data
+
+display all the data at once
+
+    taskFactory.get     getTask( 'twitter' )      */
--- src/goatstone/remote/task/weather-remote.js
+++ src/goatstone/remote/task/weather-remote.js
@@ -1,12 +1,28 @@
 /* goatstone.remote.task.weatherRemote */
 
+var config = require( 'goatstone/config' )
+
 const weatherRemote = {
-	id: 99999999999,
+	id: config.openWeatherMapKey ,
+	q: 'London', // TODO use getter / setter here
 	task:   cb => {
-		setTimeout( () =>{ 
-			console.log('weatherRemote', weatherRemote.id)  		
+		console.log( 'qqq', weatherRemote.q)
+		const axios = require( 'axios' )
+
+		axios.get('http://api.openweathermap.org/data/2.5/weather' +
+			'?q='+ 
+			weatherRemote.q + 
+			',&appid=' + 
+			weatherRemote.id )
+		  .then(function (response) {
+		  	const br = response.data 
+		    console.log( br.wind );
 			cb() 
-		}, 2000 ) 
+		  })
+		  .catch(function (response) {
+		    console.log(response);
+		  });
+ 
 	} }
 
 module.exports = weatherRemote
