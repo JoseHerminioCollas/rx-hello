7ec6adc581e4365ad607b7aca1d2311e233e55be
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-03-26 19:53:09 -0700

    Add twitter service to backend.

--- serve/app.js
+++ serve/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var path = require('path');
 var express = require('express');
 var config = require('./config')();
+var appConfig = require( '../src/goatstone/config').twitter
 var app = express();
 app.disable('etag');
-
+var TwitterRemote = require( './remote/twitter' )
 var host = '192.168.0.3'
 var port = 8080
 
@@ -13,8 +13,16 @@ app.use(express.static( __dirname + '/public'));
 app.get('/', function (req, res) {
   res.sendFile( __dirname + '/public/index.html' )
 });
-
+app.get('/weather/twitter', function( req, res ){
+    var twitter = new TwitterRemote( appConfig )
+    twitter.getSearch(
+        {
+            'q':'detroit weather',
+            'count': 10
+        }, TwitterRemote.error, function( data ){
+            res.send( data )
+        })
+})
 var server = app.listen( 8080, function () {
-  console.log('App listening at http://%s:%s', host, port);
+  console.log('app listening at http://%s:%s', host, port);
 });
-
