87dda402d2f1121a5c598b59a820d3875c066a6e
joseherminiocollas <joseherminiocollas@goatstone.com>
2020-10-18 20:33:34 -0700

    make object, call object, update vscode config

--- src/goatstone/stream.js
+++ src/goatstone/stream.js
@@ -1,23 +1,34 @@
 var rx = require('rxjs');
 
-const source = ['AAA', 'BBB'];
-const subject$ = new rx.Subject()
-
-const display = () => {
-    const onM$ = rx.zip(
-        rx.of(...source),
-        rx.timer(0, 1000)
-    )
-    .subscribe(([e]) => {
-        subject$.next(e)
-    })
-    source.splice(0)
+const Message = () => {
+    const source = ['AAA', 'BBB'];
+    const subject$ = new rx.Subject()
+    const display = () => {
+        const onM$ = rx.zip(
+            rx.of(...source),
+            rx.timer(0, 1000)
+        )
+        .subscribe(([e]) => {
+            subject$.next(e)
+        })
+        source.splice(0)
+    }
+    const onMessage = listener => {
+        subject$.asObservable().subscribe(listener)
+    }
+    const add = message => {
+        source.push(message)
+       display()
+    }
+    return {
+        display,
+        add,
+        onMessage
+    }
 }
-const onMessage = listener => {
-    subject$.asObservable().subscribe(listener)    
-}
-onMessage(evnt => console.log('ev', evnt))
-display()
-source.push('CCC')
+const message = Message()
+message.onMessage(evnt => console.log('ev', evnt))
 
-setTimeout(() => display(), 3000)
+setTimeout(() => message.add('CCC'), 3000)
+setTimeout(() => message.add('DDD'), 6000)
+setTimeout(() => message.add('e'), 6000)
