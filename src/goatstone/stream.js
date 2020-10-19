189b802ae6386cce33ad1812d612fad2c61b736b
joseherminiocollas <joseherminiocollas@goatstone.com>
2020-10-18 19:53:52 -0700

    add a stream timer and adding functionality

--- src/goatstone/stream.js
+++ src/goatstone/stream.js
@@ -1,35 +1,23 @@
-'use strict'
-var Rx = require('rxjs/Rx');
+var rx = require('rxjs');
 
-let isReadyG = new Rx.Subject()
-let data = new Rx.Subject()
-let isReady = new Rx.Subject()
-let drawData = isReady
-.combineLatest(data, isReadyG, (x, x1) => x1)
+const source = ['AAA', 'BBB'];
+const subject$ = new rx.Subject()
 
-drawData.subscribe((x)=>{
-    console.log('k ', x)
-})
+const display = () => {
+    const onM$ = rx.zip(
+        rx.of(...source),
+        rx.timer(0, 1000)
+    )
+    .subscribe(([e]) => {
+        subject$.next(e)
+    })
+    source.splice(0)
+}
+const onMessage = listener => {
+    subject$.asObservable().subscribe(listener)    
+}
+onMessage(evnt => console.log('ev', evnt))
+display()
+source.push('CCC')
 
-isReady.next() // Need to call this to get it started
-
-data.next({a: 2})
-data.next({a: 3})
-
-isReadyG.next(true)
-
-data.next({a: 4})
-
-var source = Rx.Observable
-    .interval(1500)
-    .timeInterval()
-    .take(5);
-
-var subscription = source.subscribe(
-    (x) => {
-        let n = Math.random()
-        data.next({a: n})
-    },
-    (err) => { console.log('Error: ' + err) },
-    () => { console.log('Completed') }
-)
+setTimeout(() => display(), 3000)
